c81965b9e345405db6b1b56b0b8912c061a58462
abhijeetTehra <139863973+abhijeetTehra@users.noreply.github.com>
2024-12-26 08:47:46 +0530

    Update getBalance.js

--- getBalance.js
+++ getBalance.js
@@ -1,6 +1,8 @@
 const { ethers } = require("ethers");
 const { wallet, erc20Abi, tokenAddress } = require("./config");
 
+const recipient = process.argv[5]; // Replace with the recipient's address
+
 async function main() {
   const tokenContract = new ethers.Contract(tokenAddress, erc20Abi, wallet);
   const myAddress = wallet.address; // Your wallet address
@@ -10,7 +12,6 @@ async function main() {
   console.log(`Your balance: ${ethers.utils.formatUnits(balance, 18)} tokens`);
 
   // Transfer tokens
-  const recipient = "0x77231Bbe719a9220E5d1e87CC1B09A3ca733422f"; // Replace with the recipient's address
   const amount = ethers.utils.parseUnits("10", 18); // Replace with the token amount to transfer
 
   console.log(`Transferring ${ethers.utils.formatUnits(amount, 18)} tokens to ${recipient}...`);
